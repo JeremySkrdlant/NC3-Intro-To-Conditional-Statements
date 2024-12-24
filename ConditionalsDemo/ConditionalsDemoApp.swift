c2be6d908ebf3bc376d24002a07769cfed40f9e8
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2024-12-24 06:41:49 -0700

    Initial Commit

---
+++ ConditionalsDemo/ConditionalsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConditionalsDemoApp.swift
+//  ConditionalsDemo
+//
+//  Created by Jeremy Skrdlant on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ConditionalsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConditionalsDemoTests/ConditionalsDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConditionalsDemoTests.swift
+//  ConditionalsDemoTests
+//
+//  Created by Jeremy Skrdlant on 12/24/24.
+//
+
+import Testing
+@testable import ConditionalsDemo
+
+struct ConditionalsDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
